5fe0d9d6abc5d45166537a32df53496ee54890b0
Agustín Ruatta <agustinruatta@gmail.com>
2022-04-27 22:01:33 -0300

    Añadido historial de operaciones

--- script.js
+++ script.js
@@ -5,34 +5,42 @@ document.getElementById('botonCalcular').addEventListener('click', (e) => {
     let numeroB = parseFloat(document.getElementById('numeroB').value);
     let operacion = document.getElementById('selectorOperacion').value;
     let resultado;
+    let textoOperacion;
 
     switch (operacion) {
         case 'suma':
             resultado = numeroA + numeroB;
+            textoOperacion = numeroA + ' + ' + numeroB;
             break;
 
         case 'resta':
             resultado = numeroA - numeroB;
+            textoOperacion = numeroA + ' - ' + numeroB;
             break;
 
         case 'multiplicacion':
             resultado = numeroA * numeroB;
+            textoOperacion = numeroA + ' * ' + numeroB;
             break;
 
         case 'division':
             resultado = numeroA / numeroB;
+            textoOperacion = numeroA + ' division ' + numeroB;
             break;
 
         case 'potencia':
             resultado = Math.pow(numeroA, numeroB);
+            textoOperacion = numeroA + ' ^ ' + numeroB;
             break;
 
         case 'raiz':
             resultado = Math.pow(numeroA, 1 / numeroB);
+            textoOperacion = numeroA + ' // ' + numeroB;
             break;
 
         case 'modulo':
             resultado = numeroA % numeroB;
+            textoOperacion = numeroA + ' % ' + numeroB;
             break;
     }
 
@@ -41,4 +49,8 @@ document.getElementById('botonCalcular').addEventListener('click', (e) => {
 
     document.getElementById('resultado').innerHTML = '';
     document.getElementById('resultado').appendChild(etiquetaP);
+
+    let operacionHistorial = document.createElement('p');
+    operacionHistorial.appendChild(document.createTextNode(textoOperacion));
+    document.getElementById('historial').appendChild(operacionHistorial);
 })
